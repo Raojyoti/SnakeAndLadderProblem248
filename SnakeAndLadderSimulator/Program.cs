965b3c53af2f64d1f4d20615e9928f7ed0772d5f
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-24 13:34:54 +0530

    [Jyoti Rao] Player rolls the dice to get a number between 1 to 6.

--- SnakeAndLadderSimulator/Program.cs
+++ SnakeAndLadderSimulator/Program.cs
@@ -6,9 +6,32 @@
         {
             Console.WriteLine("Welcome to the snake and ladder game");
             Console.WriteLine("---------------------------------------");
-            Game game= new Game();  
-            game.GameStart();
-            Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Please select given options");
+                Console.WriteLine("1.Player1StartPosition\n2.Player1RollsDice\n8.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                Game game = new Game();
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        game.GameStart();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        game.PlayerRollsDice();
+                        break;
+                    case 8:
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Select only given options ");
+                        break;
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
